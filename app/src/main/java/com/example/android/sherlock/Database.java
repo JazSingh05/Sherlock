0d27bf4e7bb600606ccff2bda2b661f8db97033e
miedmond <montycharlie2@hotmail.com>
2018-02-14 16:55:39 -0700

    Issue: #19 Implemented basic database functionality

--- app/src/main/java/com/example/android/sherlock/Database.java
+++ app/src/main/java/com/example/android/sherlock/Database.java
@@ -2,8 +2,12 @@ package com.example.android.sherlock;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by Abbinav, Jaz on 1/28/2018.
@@ -17,47 +21,83 @@ public class Database extends SQLiteOpenHelper {
 
     private static final String ID = "id";
     private static final String NAME = "storeName";
-    private static final String PRODUCTID = "productID";
+    private static final String ADDRESS = "address";
     private static final String PRODUCTNAME = "productNAME";
     private static final String PRODUCTPICTURE = "productPicture";
     private static final String PRICE = "price";
+
 
     public Database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
-        String CREATE_STORE_TABLE = "CREATE TABLE" + TABLE_ITEMS + "(" + ID + "INTEGER PRIMARY KEY," +
-                                    NAME + "VARCHAR" + PRODUCTID + "INTEGER PRIMARY KEY" + PRODUCTNAME + "VARCHAR"
-                                    + PRODUCTPICTURE + "BLOB" + PRICE + "DOUBLE" + ")";
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        String CREATE_STORE_TABLE = "CREATE TABLE " + TABLE_ITEMS + "(" + ID + " INTEGER," +
+                                    NAME + " VARCHAR," + ADDRESS + " VARCHAR," + PRODUCTNAME + " VARCHAR,"
+                                    + PRICE + " DOUBLE," + " PRIMARY KEY" + "(" + ID +"," + NAME +
+                                    "," + ADDRESS + "," + PRODUCTNAME + "," + PRICE + ") " +
+                                    ")";
 
         sqLiteDatabase.execSQL(CREATE_STORE_TABLE);
-
-
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS" + TABLE_ITEMS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEMS);
         onCreate(sqLiteDatabase);
-
     }
 
     public void addStore(Store store){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(ID, store.getId());
-        values.put(NAME, store.getStoreName());
-        values.put(PRODUCTID, store.getProductId());
-        values.put(PRODUCTNAME, store.getProductName());
-      //  values.put(PRODUCTPICTURE, String.valueOf(store.getProductPicture()));
-        values.put(PRICE, store.getPrice());
 
-        db.insert(TABLE_ITEMS, null, values);
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(ID, "7");
+        values.put(NAME, "Costco");
+        values.put(ADDRESS, "123 Fake Street");
+        values.put(PRODUCTNAME, "apple");
+        values.put(PRICE, "1.99");
+
+        long result = db.insert(TABLE_ITEMS, null, values);
+        Log.d("STORAGE_CHECK", "insertdata: Added " +   " to " + "\nResult = " + result);
         db.close();
+    }
+
+    public ArrayList<String> searchQuery(String s)
+    {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {ID, NAME, ADDRESS, PRODUCTNAME, PRICE};
+
+        Cursor cursor = db.query(TABLE_ITEMS, projection, null, null, null, null, null);
+        cursor.moveToLast();
+
+        int index = 6;
+        ArrayList<String> queries = new ArrayList<String>();
+
+        for(int i = 0; i < 7; i++)
+        {
+            String temp = "";
+            for(int j = 0; j < 5; j++)
+            {
+                temp = temp + " " +cursor.getString(j);
+            }
+
+            index--;
+            cursor.moveToPosition(index);
+
+            queries.add(temp);
+        }
+
+        for(int i = 0; i < 7; i++)
+        {
+            Log.d("RECIEVED: ", "TEST = " + queries.get(i));
+        }
+
+        return queries;
     }
 }
--- app/src/main/java/com/example/android/sherlock/SearchActivity.java
+++ app/src/main/java/com/example/android/sherlock/SearchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,17 +23,30 @@ public class SearchActivity extends AppCompatActivity {
 
     public void searchDatabase(View view)
     {
+
+        Database db = new Database(this);
+        Store mystore = new Store("2", "Ralphs", "123 Fake St", "apple", "1.99");
+        db.addStore(mystore);
+
         EditText editText = (EditText) findViewById(R.id.editText);
         if(!editText.getText().toString().matches("") || editText.getText().toString().matches("")) {
             String DatabaseSearch = editText.getText().toString();
+            Log.d("CHECKING STRING", "THe string you entered: " + DatabaseSearch);
             Intent intent = new Intent(this, DatabaseResult.class);
-            startActivity(intent);
             intent.putExtra("sherlock.com.sherlock", DatabaseSearch);
+            startActivity(intent);
+
         }
         else
         {
             Toast.makeText(this, "Enter in Something", Toast.LENGTH_SHORT).show();
         }
+
+
+
+
+
+
     }
 
 

c3b5f5d66df6398be175dca837c4a4a4603ac899
Kyle <kyle.taggard96@gmail.com>
2018-03-02 11:05:59 -0800

    Issue #27 Update Settings Layout

--- app/src/main/java/com/example/android/sherlock/activity/Settings.java
+++ app/src/main/java/com/example/android/sherlock/activity/Settings.java
@@ -9,6 +9,8 @@ import com.example.android.sherlock.R;
 
 public class Settings extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +18,7 @@ public class Settings extends AppCompatActivity {
     }
 
 
-    public void mainScreen(View view){
+    public void settingsMain(View view){
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
